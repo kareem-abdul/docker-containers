b9e3086c8cbcef4c33dba307402404decfd6510b
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-28 23:45:59 +0530

    feat: adds a script to start containers

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# requires fzf to be installed
+
+find . -type f -iname "docker-compose*" 2>/dev/null \
+    | sed -E 's|^./(.*)$|\1|g' \
+    | fzf -m -i --layout=reverse --height=20 --border=rounded --border-label="Choose service" --info=right --preview='less {}' \
+    | while read -r composefile; do
+        which docker-compose &> /dev/null;
+        if [ $? -eq 0 ]; then 
+            docker-compose -f "$composefile" up --build -d
+            exit 0;
+        fi
+
+        # check if docker compose plugin is installed
+        docker compose version &> /dev/null
+        if [ $? -eq 0 ]; then 
+            docker compose -f "$composefile" up --build -d
+            exit 0;
+        fi
+        echo "docker compose not found" 1>&2;
+        exit 1;
+    done
+
