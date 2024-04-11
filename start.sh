da57d5406203139f87d6f5e3d25e94bed1c71154
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-04-11 23:25:53 +0530

    feat: adds podman support to start script

--- start.sh
+++ start.sh
@@ -1,6 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # requires fzf to be installed
+
+shopt -s expand_aliases
+
+
+COMPOSE=0
+if which docker &> /dev/null; then
+    if docker compose version &> /dev/null; then
+        alias compose="docker compose"
+        COMPOSE=1
+    elif which docker-compose &> /dev/null; then
+        alias compose="docker-compose"
+        COMPOSE=1
+    fi
+elif which podman &> /dev/null; then
+    if which docker-compose &> /dev/null || which podman-compose &> /dev/null; then
+        alias compose="podman compose 2> /dev/null"
+        COMPOSE=1
+    fi
+fi
+
+if [ $COMPOSE -eq 0 ]; then
+    echo "docker compose not available";
+    exit 1;
+fi
+
 
 DIR=$(dirname "$(readlink -f "$0")")
 cd $DIR
@@ -9,19 +34,7 @@ find . -type f -iname "docker-compose*" 2>/dev/null \
     | sed -E 's|^./(.*)$|\1|g' \
     | fzf -m -i --layout=reverse --height="100%" --border=rounded --border-label="Choose service" --info=right --preview='less {}' \
     | while read -r composefile; do
-        which docker-compose &> /dev/null;
-        if [ $? -eq 0 ]; then 
-            docker-compose -f "$composefile" up --build -d
-            exit 0;
-        fi
-
-        # check if docker compose plugin is installed
-        docker compose version &> /dev/null
-        if [ $? -eq 0 ]; then 
-            docker compose -f "$composefile" up --build -d
-            exit 0;
-        fi
-        echo "docker compose not found" 1>&2;
-        exit 1;
+        compose -f "$composefile" up --build -d
+        sleep 2;
     done
 
